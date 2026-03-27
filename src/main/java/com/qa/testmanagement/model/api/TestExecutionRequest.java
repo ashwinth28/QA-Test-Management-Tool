c314b7e5241d88514757e7bfe7b3e08ccf7bd519
ashwinth28 <antonyashwinth1999@gmail.com>
2026-03-28 03:10:54 +0530

    Fix: Add missing getters and setters in API model classes

--- src/main/java/com/qa/testmanagement/model/api/TestExecutionRequest.java
+++ src/main/java/com/qa/testmanagement/model/api/TestExecutionRequest.java
@@ -1,5 +1,52 @@
 package com.qa.testmanagement.model.api;
 
-public class TestExecutionRequest {
+import com.qa.testmanagement.model.TestStatus;
 
+public class TestExecutionRequest {
+    private Long testCaseId;
+    private String executedBy;
+    private String actualResult;
+    private TestStatus status;
+    private String remarks;
+
+    // Getters and Setters
+    public Long getTestCaseId() {
+        return testCaseId;
+    }
+
+    public void setTestCaseId(Long testCaseId) {
+        this.testCaseId = testCaseId;
+    }
+
+    public String getExecutedBy() {
+        return executedBy;
+    }
+
+    public void setExecutedBy(String executedBy) {
+        this.executedBy = executedBy;
+    }
+
+    public String getActualResult() {
+        return actualResult;
+    }
+
+    public void setActualResult(String actualResult) {
+        this.actualResult = actualResult;
+    }
+
+    public TestStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TestStatus status) {
+        this.status = status;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
 }
